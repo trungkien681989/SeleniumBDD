0f02f1f36eaf904741625101b56c8a90a43e8f25
Kien <Kien@PC>
2019-06-08 23:00:10 +0700

    Update DriverManager

--- src/test/java/driverFactory/ChromeHeadlessDriverManager.java
+++ src/test/java/driverFactory/ChromeHeadlessDriverManager.java
@@ -3,12 +3,15 @@ package driverFactory;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class ChromeHeadlessDriverManager extends DriverManager{
 	
 	@Override
 	public void createWebDriver() {
-    	System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/win/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
+		//System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/win/chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         this.driver = new ChromeDriver(options);
 	}
--- src/test/java/driverFactory/FirefoxHeadlessDriverManager.java
+++ src/test/java/driverFactory/FirefoxHeadlessDriverManager.java
@@ -2,12 +2,15 @@ package driverFactory;
 
 import org.openqa.selenium.firefox.*;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class FirefoxHeadlessDriverManager extends DriverManager{
 	
 	@Override
 	public void createWebDriver() {
-    	System.setProperty("webdriver.gecko.driver","src/test/resources/drivers/win/geckodriver.exe");
-        FirefoxOptions options = new FirefoxOptions();
+    	//System.setProperty("webdriver.gecko.driver","src/test/resources/drivers/win/geckodriver.exe");
+    	WebDriverManager.firefoxdriver().setup();
+    	FirefoxOptions options = new FirefoxOptions();
         options.addArguments("--headless");
         this.driver = new FirefoxDriver(options);
 	}
